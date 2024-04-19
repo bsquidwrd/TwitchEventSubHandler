f1bbd825e145c90babc3852b2fab2a0f78bf479c
bsquidwrd <git@bsquid.io>
2024-04-18 22:12:43 -0700

    Perform cost check

--- main.go
+++ main.go
@@ -60,6 +60,25 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	)
 
 	w.Header().Add("Content-Type", "text/plain")
+
+	// Perform cost check
+	// If there's a cost with a revocation, that's okay
+	// We'll be cleaning it up anyway
+	if r.Header.Get("Twitch-Eventsub-Message-Type") != "revocation" {
+		var eventsubMessage models.EventsubMessage
+		err = json.Unmarshal(rawBody, &eventsubMessage)
+		if err != nil {
+			slog.Error("Could not unmarshal body", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if eventsubMessage.Subscription.Cost > 0 {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Fprint(w, "That's too rich for my blood")
+			return
+		}
+	}
+
 	switch r.Header.Get("Twitch-Eventsub-Message-Type") {
 
 	case "webhook_callback_verification":
