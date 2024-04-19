a8057b33c63de882bc7c0e3494c7a9ea0f74cdfd
bsquidwrd <git@bsquid.io>
2024-04-19 15:39:41 -0700

    Log healthcheck calls

--- routes/healthcheck.go
+++ routes/healthcheck.go
@@ -2,10 +2,17 @@ package routes
 
 import (
 	"fmt"
+	"html"
+	"log/slog"
 	"net/http"
 )
 
 func HandleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	slog.Info(
+		"Healthcheck called",
+		"endpoint", html.EscapeString(r.URL.Path),
+	)
+
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "OK")
 }
