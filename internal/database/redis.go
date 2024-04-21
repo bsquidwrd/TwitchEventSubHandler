368f63c44f21dbd98593e25bc7220858b475a35f
bsquidwrd <git@bsquid.io>
2024-04-20 17:05:55 -0700

    Add locking functions and get/set bool

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
 	"time"
@@ -32,6 +33,36 @@ func newRedisService() *redisService {
 	}
 }
 
+func (r *redisService) TakeLock(key string, value string, expiration time.Duration) bool {
+	lockKey := fmt.Sprintf("lock:%s", key)
+	// Have a reasonable default a lock can be taken for
+	if expiration > (1 * time.Minute) {
+		expiration = 1 * time.Minute
+	}
+	result, err := r.db.SetNX(context.Background(), lockKey, value, expiration).Result()
+	if err != nil {
+		return false
+	}
+	return result
+}
+
+func (r *redisService) ReleaseLock(key string, value string) bool {
+	lockKey := fmt.Sprintf("lock:%s", key)
+	existingValue := r.GetString(lockKey)
+	if value == existingValue {
+		return r.Delete(lockKey)
+	}
+	return false
+}
+
+func (r *redisService) Delete(key string) bool {
+	err := r.db.Del(context.Background(), key).Err()
+	if err != nil {
+		slog.Error("Error settings key in Redis", err)
+	}
+	return err != nil
+}
+
 func (r *redisService) GetString(key string) string {
 	value, err := r.db.Get(context.Background(), key).Result()
 	switch {
@@ -46,7 +77,26 @@ func (r *redisService) GetString(key string) string {
 func (r *redisService) SetString(key string, value string, expiration time.Duration) bool {
 	err := r.db.Set(context.Background(), key, value, expiration).Err()
 	if err != nil {
+		slog.Error("Error settings key in Redis", err)
+	}
+	return err != nil
+}
+
+func (r *redisService) GetBool(key string) bool {
+	value, err := r.db.Get(context.Background(), key).Bool()
+	switch {
+	case err == redis.Nil:
+		value = false
+	case err != nil:
 		slog.Error("Error getting key from Redis", err)
+	}
+	return value
+}
+
+func (r *redisService) SetBool(key string, value bool, expiration time.Duration) bool {
+	err := r.db.Set(context.Background(), key, value, expiration).Err()
+	if err != nil {
+		slog.Error("Error settings key in Redis", err)
 	}
 	return err != nil
 }
