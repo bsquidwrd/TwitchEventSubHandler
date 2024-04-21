8c4fd480630ecb44748152eae7c48f8c2fe8c232
bsquidwrd <git@bsquid.io>
2024-04-21 14:53:58 -0700

    On grant, subscribe to the 4 types of subscriptions we care about

--- internal/handlers/authorization_grant.go
+++ internal/handlers/authorization_grant.go
@@ -1,12 +1,29 @@
 package handlers
 
 import (
+	"fmt"
 	"log/slog"
+	"net/http"
+	"os"
 
 	"github.com/bsquidwrd/TwitchEventSubHandler/internal/database"
 	"github.com/bsquidwrd/TwitchEventSubHandler/internal/models"
+	"github.com/bsquidwrd/TwitchEventSubHandler/internal/twitch"
 )
 
 func processAuthorizationGrant(dbServices *database.Service, notification *models.AuthorizationGrantEventMessage) {
 	slog.Info("User granted authorization", "userid", notification.Event.UserID)
+
+	eventsubSecret := os.Getenv("EVENTSUBSECRET")
+	eventsubWebhook := os.Getenv("EVENTSUBWEBHOOK")
+	bodies := []string{
+		fmt.Sprintf(`{"type":"user.update","version":"1","condition":{"user_id":"%s"},"transport":{"method":"webhook","callback":"%s","secret":"%s"}}`, notification.Event.UserID, eventsubWebhook, eventsubSecret),
+		fmt.Sprintf(`{"type":"channel.update","version":"2","condition":{"broadcaster_user_id":"%s"},"transport":{"method":"webhook","callback":"%s","secret":"%s"}}`, notification.Event.UserID, eventsubWebhook, eventsubSecret),
+		fmt.Sprintf(`{"type":"stream.online","version":"1","condition":{"broadcaster_user_id":"%s"},"transport":{"method":"webhook","callback":"%s","secret":"%s"}}`, notification.Event.UserID, eventsubWebhook, eventsubSecret),
+		fmt.Sprintf(`{"type":"stream.offline","version":"1","condition":{"broadcaster_user_id":"%s"},"transport":{"method":"webhook","callback":"%s","secret":"%s"}}`, notification.Event.UserID, eventsubWebhook, eventsubSecret),
+	}
+
+	for _, body := range bodies {
+		go twitch.CallApi(dbServices, http.MethodPost, "eventsub/subscriptions", body, nil)
+	}
 }
