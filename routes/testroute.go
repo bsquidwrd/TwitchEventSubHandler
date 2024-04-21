75acadd7079997d47c2a5f5d80cf78de86f16135
bsquidwrd <git@bsquid.io>
2024-04-20 17:10:45 -0700

    Add a test route for testing hte API calls

---
+++ routes/testroute.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/bsquidwrd/TwitchEventSubHandler/internal/database"
+	"github.com/bsquidwrd/TwitchEventSubHandler/internal/twitch"
+)
+
+func HandleTestRoute(dbServices *database.Services) func(http.ResponseWriter, *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		parameters := r.URL.Query()
+		endpoint := parameters.Get("endpoint")
+		parameters.Del("endpoint")
+
+		if endpoint == "" {
+			endpoint = "helix/users"
+		}
+		if len(parameters) == 0 {
+			parameters.Set("login", "bsquidwrd")
+		}
+
+		result, err := twitch.CallApi(dbServices, endpoint, "", &parameters)
+		if err != nil {
+			slog.Error("Error calling API", err)
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprint(w, string(result))
+	}
+}
