7fcebd7173ea7d7dec8a1ad799f3d6ad13351373
bsquidwrd <git@bsquid.io>
2024-04-26 15:52:09 -0700

    Fix vet warning

--- healthcheck/main.go
+++ healthcheck/main.go
@@ -12,10 +12,10 @@ func main() {
 		port = "8080"
 	}
 	response, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s/healthcheck", port))
-	defer response.Body.Close()
 	if err != nil {
 		os.Exit(1)
 	}
+	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
 		os.Exit(1)
