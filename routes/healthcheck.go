d4dde0c32f071cb984400f4b0268ace6846efbdc
bsquidwrd <git@bsquid.io>
2024-04-26 17:52:01 -0700

    Check DB ping for healthcheck

--- routes/healthcheck.go
+++ routes/healthcheck.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -9,7 +10,13 @@ import (
 
 func HandleHealthCheck(dbServices *database.Service) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusUnauthorized)
+		err := dbServices.Database.Ping(context.Background())
+		if err != nil {
+			http.Error(w, "ERROR", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "OK")
 	}
 }
