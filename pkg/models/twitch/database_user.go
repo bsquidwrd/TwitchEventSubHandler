5c9b3d7540e4356a0fcb4ad9ea81668ea9913aaf
bsquidwrd <git@bsquid.io>
2024-04-28 00:40:59 -0700

    Add defaults for all fields except timestamps, which are nullable

--- pkg/models/twitch/database_user.go
+++ pkg/models/twitch/database_user.go
@@ -1,19 +1,22 @@
 package twitch
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type DatabaseUser struct {
-	Id            string
-	Name          string
-	Login         string
-	Email         string
-	EmailVerified bool
-	Description   string
-	Title         string
-	Language      string
-	CategoryID    string
-	CategoryName  string
-	LastOnlineAt  time.Time
-	LastOfflineAt time.Time
-	Live          bool
+	Id            string              `json:"id"`
+	Name          string              `json:"name"`
+	Login         string              `json:"login"`
+	Email         string              `json:"email,omitempty"`
+	EmailVerified bool                `json:"email_verified,omitempty"`
+	Description   string              `json:"description,omitempty"`
+	Title         string              `json:"title,omitempty"`
+	Language      string              `json:"language,omitempty"`
+	CategoryId    string              `json:"category_id,omitempty"`
+	CategoryName  string              `json:"category_name,omitempty"`
+	LastOnlineAt  sql.Null[time.Time] `json:"last_online_at,omitempty"`
+	LastOfflineAt sql.Null[time.Time] `json:"last_offline_at,omitempty"`
+	Live          bool                `json:"live,omitempty"`
 }
