1f63934f2a332057a8dc76dda8f6b0d6f6851ee5
bsquidwrd <git@bsquid.io>
2024-05-02 20:58:56 -0700

    Conflict is an okay status code for API calls

--- internal/twitch/api.go
+++ internal/twitch/api.go
@@ -251,7 +251,7 @@ func CallApi(dbServices *database.ReceiverService, method string, endpoint strin
 			return 0, nil, err
 		}
 
-		if response.StatusCode >= 300 || response.StatusCode < 200 {
+		if response.StatusCode != http.StatusConflict && (response.StatusCode >= 300 || response.StatusCode < 200) {
 			slog.Error("API returned non 2xx code", "endpoint", endpoint, "status_code", response.StatusCode, "response", string(rawBody))
 			return response.StatusCode, rawBody, errors.New("status code is not 2xx")
 		}
